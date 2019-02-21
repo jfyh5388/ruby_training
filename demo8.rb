cbac06f3b6c94a29ab86a0d9cf236f88834e45af
Chao Yang <yangchao5388@163.com>
2019-02-21 09:35:07 +0800

    Update demo8.rb

--- demo8.rb
+++ demo8.rb
@@ -13,18 +13,21 @@ class Shape
     end
 end
 class Rectangle < Shape
-=begin
     def initialize(w,h,name) 
         @width = w
         @height = h 
         #super(w)
         @name = name
     end 
-=end
 
-    def new_va(name)
-        @name = name
+    def new_price(price)
+        @price = price
     end
+
+    def get_price
+        @price
+    end
+
     def area
         @width*@height
     end
@@ -48,8 +51,9 @@ shape1 = Shape.new(4,6)
 puts shape1.width
 puts shape1.to_s
 #puts shape1.height
-rec1 = Rectangle.new(5,6)
-rec1.new_va("abc")
+rec1 = Rectangle.new(5,6,"rec1")
+rec1.new_price(20)
+puts rec1.get_price
 puts rec1.area
 puts rec1.to_s
 cir1 = Circle.new(2)
